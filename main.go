a88e40d703d5f1a133567afab17a3e7abd168641
dark-person <66205094+dark-person@users.noreply.github.com>
2022-12-07 23:35:55 +0800

    Latest Update

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"sort"
 	"strconv"
+	"sync"
 	"time"
 
 	"gopkg.in/ini.v1"
@@ -38,6 +40,7 @@ func main() {
 
 	// Get Task Number
 	task_count := config.Section("").Key("task_count").MustInt()
+	// version_number := config.Section("").Key("last_updated").MustInt()
 
 	for i := 0; i < task_count; i++ {
 		section := strconv.Itoa(i + 1)
@@ -60,24 +63,6 @@ func main() {
 
 		// Parse the config to object
 		task_list = append(task_list, schedule{task_id: i + 1, twitch_link: twitch_link, duration: duration_minute, start_time: until})
-
-		// fmt.Println(until.String())
-		// fmt.Println(time.Now().String())
-
-		// fmt.Println(until.Equal(time.Now()))
-		// fmt.Println(until.Before(time.Now()))
-		// fmt.Println(until.After(time.Now()))
-
-		// if until.Before(time.Now()) {
-		// 	fmt.Println("Task Outdated.")
-		// 	os.Exit(1)
-		// }
-
-		// time.Sleep(time.Until(until))
-		// fmt.Println("Task Completed.")
-
-		// //exec.Command("cmd.exe", "/C", "twitch_drop_general.bat", twitch_link, strconv.Itoa(duration_minute))
-		// break
 	}
 
 	// Sort the object due to time order
@@ -96,30 +81,107 @@ func main() {
 
 	fmt.Println(task_list)
 
+	// Prepare worker pool
+	concurrentGoroutines := make(chan struct{}, 5)
+
+	var wg sync.WaitGroup
+
 	// Start Main loop
 	for _, item := range task_list {
-		// Notify the outdated task
-		if item.start_time.Before(time.Now()) {
-			fmt.Println("Task", item.task_id, "Outdated.")
-			continue
-		}
+		// Wait Group Increment
+		wg.Add(1)
 
+		go func(item schedule) {
+			// Defer Done() for WaitGroup
+			defer wg.Done()
+			concurrentGoroutines <- struct{}{}
+
+			// Notify the outdated task
+			if item.start_time.Before(time.Now()) {
+				fmt.Println("Task", item.task_id, "Outdated.")
+				<-concurrentGoroutines
+				return
+			}
+
+			// Sleep until time come
+			// fmt.Println("Sleeping...")
+			time.Sleep(time.Until(item.start_time))
+
+			// Time matched, start the batch script with parameters
+			cmd1 := exec.Command("cmd.exe", "/C", "start", "https://www.twitch.tv/drops/inventory")
+			cmd1.Start()
+			time.Sleep(time.Duration(10) * time.Second)
+
+			cmd2 := exec.Command("cmd.exe", "/C", "start", item.twitch_link)
+			cmd2.Start()
+
+			// Wait the duration
+			// fmt.Println("Sleeping...")
+			time.Sleep(time.Duration(item.duration) * time.Minute)
+
+			<-concurrentGoroutines
+		}(item)
 	}
 
-	// Sleep until time come
-
-	// Time matched, start the batch script with parameters
-
-	// Wait the batch script stop
-
-	// Prepare the next task (loop counter increment)
+	wg.Wait()
 
 	// Notify No Task is pending
+	fmt.Println("All Task is completed.")
 
-	// Fetch The Ini text setting from web?
+	// // Fetch The Ini text setting from web
+	// source_ini_link := "https://raw.githubusercontent.com/dark-person/Twitch-drop-custom/master/schedule.ini"
+	// fileURL, err := url.Parse(source_ini_link)
+	// if err != nil {
+	// 	fmt.Println("Download INI Failed:", err)
+	// }
+	// path := fileURL.Path
+	// segments := strings.Split(path, "/")
+	// fileName := "TEMP_" + segments[len(segments)-1]
 
-	//fmt.Println("Twitch Link:", twitch_link)
-	//fmt.Println("Duration:", duration_minute, "minute")
+	// // Create blank file
+	// file, err := os.Create(fileName)
+	// if err != nil {
+	// 	fmt.Println("Create Ini Failed:", err)
+	// }
+	// client := http.Client{
+	// 	CheckRedirect: func(r *http.Request, via []*http.Request) error {
+	// 		r.URL.Opaque = r.URL.Path
+	// 		return nil
+	// 	},
+	// }
+	// // Put content on file
+	// resp, err := client.Get(source_ini_link)
+	// if err != nil {
+	// 	fmt.Println("Download INI Failed:", err)
+	// }
+	// defer resp.Body.Close()
 
-	// Prepare to start the bat
+	// size, _ := io.Copy(file, resp.Body)
+	// defer file.Close()
+
+	// fmt.Printf("Downloaded ini %s with size %d\n", fileName, size)
+
+	// // Verify the downloaded file is newer than current	version
+	// // Read the config from external files
+	// new_config, err := ini.Load("TEMP_schedule.ini")
+	// if err != nil {
+	// 	fmt.Printf("Fail to read file: %v", err)
+	// 	os.Exit(1)
+	// }
+	// new_config.SaveTo("TEMP_schedule.ini")
+
+	// // Get Task Number
+	// new_version := new_config.Section("").Key("last_updated").MustInt()
+	// fmt.Println("Downloaded Version:", new_version)
+	// if new_version > version_number {
+	// 	os.Rename("TEMP_schedule.ini", "schedule.ini")
+	// 	fmt.Println("Newer Version replaced.")
+	// } else {
+	// 	err := os.Remove("TEMP_schedule.ini")
+	// 	if err != nil {
+	// 		fmt.Printf("Remove File failed: %v\n", err)
+	// 	}
+
+	// 	fmt.Println("Downloaded Version Removed.")
+	// }
 }
